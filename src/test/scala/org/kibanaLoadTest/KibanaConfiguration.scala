b3932de4d234e4c767a140659f184c282299bd7f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2020-09-22 15:47:33 +0200

    read config from env var

--- src/test/scala/org/kibanaLoadTest/KibanaConfiguration.scala
+++ src/test/scala/org/kibanaLoadTest/KibanaConfiguration.scala
@@ -7,6 +7,8 @@ class KibanaConfiguration {
   var baseUrl = ""
   var buildVersion = ""
   var isSecurityEnabled = true
+  var login = ""
+  var password = ""
   var loginPayload = ""
 
 
@@ -15,10 +17,12 @@ class KibanaConfiguration {
     val is = getClass.getClassLoader.getResourceAsStream(configName)
     val source = scala.io.Source.fromInputStream(is).mkString
     val config = ConfigFactory.parseString(source)
-    this.baseUrl = config.getString("app.host")
-    this.buildVersion = config.getString("app.version")
+    this.baseUrl = Option(System.getenv("host")).getOrElse(config.getString("app.host"))
+    this.buildVersion = Option(System.getenv("version")).getOrElse(config.getString("app.version"))
     this.isSecurityEnabled = config.getBoolean("security.on")
-    this.loginPayload = config.getString("auth.loginPayload")
-  }
+    this.login = Option(System.getenv("login")).getOrElse(config.getString("auth.login"))
+    this.password = Option(System.getenv("password")).getOrElse(config.getString("auth.password"))
+    this.loginPayload = s"""{"username":"${this.login}","password":"${this.password}"}"""
+}
 
 }
